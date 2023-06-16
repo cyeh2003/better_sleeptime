75c4b402bae16f1e36627dfb2b2f6e0f3868fc6d
Calvin Yeh <calvinyeh@Calvins-MacBook-Pro.local>
2023-06-15 19:29:58 -0700

    Use a different color for each wakeup time.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const colors = [
+    "rgb(168, 39, 254)",
+    "rgb(154, 3, 254)",
+    "rgb(150, 105, 254)",
+    "rgb(140, 140, 255)",
+    "rgb(187, 187, 255)",
+    "rgb(143, 254, 221)",
+]
+
+function createWakeUpTimeElement(time, cycle) {
+    let elm = document.createElement("div");
+    elm.innerText = time;
+    elm.style.color = colors[cycle];
+    return elm;
+}
+
 function handleOnClickEvent() {
     let output = document.querySelector(".output");
     output.style.display = "block";
@@ -9,8 +25,9 @@ function handleOnClickEvent() {
     // allow 14 minutes to fall asleep
     now.setMinutes(now.getMinutes() + 14)
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i) {
         now.setMinutes(now.getMinutes() + 90);
-        hours.innerHTML += now.toLocaleTimeString("en-US", { timeStyle: "short" }) + "<br />";
+        const elm = createWakeUpTimeElement(now.toLocaleTimeString("en-US", { timeStyle: "short" }) , i)
+        hours.appendChild(elm);
     }
 }
